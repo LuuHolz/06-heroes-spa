5f33898061c359188d97f2256aa3f8385315f49a
Luciana <luciana.holzmann@gmail.com>
2024-07-03 00:36:47 +0200

    [FEAT] Login y logout del usuario

--- src/auth/context/AuthProvider.tsx
+++ src/auth/context/AuthProvider.tsx
@@ -1,17 +1,53 @@
 import { useReducer } from 'react';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './AuthReducer';
+import { types } from '../types/types';
 
-const initialState = {
-    logged: false,
-}
+// const initialState = {
+//     logged: false,
+// };
+
+const init = () => {
+
+    const user = JSON.parse( localStorage.getItem('user') );
+
+    return{
+        logged: !!user,
+        user: user,
+    };
+};
 
 const AuthProvider = ({ children }) => {
 
-    const [ state, dispatch ] = useReducer( authReducer, initialState )
+    const [ state, dispatch ] = useReducer( authReducer, {}, init );
+    
+
+    const login = ( name= '' ) => {
+
+        const user = { id: 'ABC', name }
+
+        const action = {
+            type: types.login,
+            payload: user
+        }
+
+        localStorage.setItem('user', JSON.stringify( user ) );
+
+        dispatch( action )
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        const action = { type: types.logout };
+        dispatch( action );
+    }
 
   return (
-        <AuthContext.Provider value = {{ }}>
+        <AuthContext.Provider value = {{ 
+            ...state,
+            login: login,
+            logout: logout
+        }}>
             { children }
         </AuthContext.Provider>
 )
--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const LoginPage = () => {
 
+  const { login } = useContext( AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
+
+    login( 'Luciana');
+
     navigate( '/', { replace: true })
   };
 
